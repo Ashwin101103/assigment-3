9b30ee442678b3b074d80beda9c8ad596a4deea2
Ashwin101103 <dr.ash0090@gmail.com>
2026-03-29 11:21:27 +0100

    Add Terraform IaC and ELK monitoring setup

---
+++ main.tf
@@ -0,0 +1,63 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the Google Cloud provider
+provider "google" {
+  credentials = file("service-account-key.json")
+  project     = var.project_id
+  region      = var.region
+  zone        = var.zone
+}
+
+# Create a compute instance
+resource "google_compute_instance" "vm_instance" {
+  name         = "lms-instance"        # This is the VM name in GCP
+  machine_type = "e2-medium"           # 2 vCPU, 4GB memory
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2204-lts"
+      size  = 20                        # Disk size in GB
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      # This enables external IP
+    }
+  }
+
+  tags = ["http-server", "https-server"]
+
+  metadata = {
+    ssh-keys = "ubuntu:${var.ssh_public_key}"
+  }
+
+  metadata_startup_script = <<-EOF
+    #!/bin/bash
+    # Just update package list initially
+    apt-get update
+  EOF
+}
+
+# Firewall rule to allow HTTP, HTTPS, and custom port
+resource "google_compute_firewall" "allow_http" {
+  name    = "allow-http"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "443", "4000", "5601", "9200"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["http-server"]
+}
